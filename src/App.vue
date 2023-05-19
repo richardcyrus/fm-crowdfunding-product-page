c44f1892d4b18a0ed60a591bfb14db7bad8caee9
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-05-19 08:45:49 -0400

    chore: install and configure Vue 3

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <main id="main" class="content">
+  </main>
+  <footer class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+    by <a href="https://www.richardcyrus.com">Richard Cyrus</a>.
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+</script>
+
+<template>
+  <header>
+    <nav id="navigation" class="navbar">
+      <div v-bind:class="{ overlay: isOpen }">
+        <div class="container">
+          <a href="#" class="navbar__brand">
+            <img src="@/assets/images/logo.svg" alt="crowdfund logo" />
+          </a>
+          <button
+            class="navbar__toggler"
+            type="button"
+            aria-controls="navbarContent"
+            aria-label="Toggle navigation"
+            v-bind:aria-expanded="isOpen.toString()"
+            v-on:click.prevent="isOpen = !isOpen"
+          >
+            <span class="navbar__toggler-icon"></span>
+          </button>
+        </div>
+        <div id="navbarContent" class="menu" v-bind:class="{ hidden: !isOpen }">
+          <a class="menu__item" href="#">About</a>
+          <a class="menu__item" href="#">Discover</a>
+          <a class="menu__item" href="#">Get Started</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
