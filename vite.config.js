0286276e60b8bb02273d65013ef1015c1699afd5
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-05-20 18:45:00 -0400

    fix: update deployment base directory

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fm-crowdfunding-product-page/',
   plugins: [vue()],
   resolve: {
     alias: {
